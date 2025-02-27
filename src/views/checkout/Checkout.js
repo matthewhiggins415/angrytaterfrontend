d6d343970fe971b70734eb700e5517194b86eecc
matthewhiggins415 <mhiggins.louis@gmail.com>
2025-02-26 16:27:29 -0800

    added s live

--- src/views/checkout/Checkout.js
+++ src/views/checkout/Checkout.js
@@ -5,23 +5,32 @@ import {
 } from '@stripe/react-stripe-js';
 import apiUrl from '../../apiConfig';
 import React, { useCallback, useState, useEffect } from "react";
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_KEY);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const CheckoutForm = () => {
   const sessionData = sessionStorage.getItem('orderInfo'); // Retrieve stored data
   const parsedData = sessionData ? JSON.parse(sessionData) : {}; // Parse if exists
 
-  const fetchClientSecret = useCallback(() => {
-    // Create a Checkout Session
-    return fetch(`${apiUrl}/create-checkout-session`, {
-      method: "POST",
-      body: JSON.stringify({
-        sessionData: parsedData, // Send session storage data
-      })
-    })
-    .then((res) => res.json())
-    .then((data) => data.clientSecret);
-  }, []);
+  const fetchClientSecret = useCallback(async () => {
+    try {
+      const response = await fetch(`${apiUrl}/create-checkout-session`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Important!
+        },
+        body: JSON.stringify({
+          sessionData: parsedData, // Send session storage data
+        }),
+      });
+  
+      const data = await response.json();
+      return data.clientSecret;
+      
+    } catch (error) {
+      console.error("Error fetching client secret:", error);
+      throw error; // Rethrow the error if needed
+    }
+  }, []);  
 
   const options = {fetchClientSecret};
   
